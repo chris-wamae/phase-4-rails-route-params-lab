f98169c4ab258116ec6c149fa15c511ee0c121d3
chris-wamae <chriswamae123@gmail.com>
2023-03-13 09:46:27 +0300

    Completed Assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,9 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def student_by_id
+    single_student = Student.find(params[:id])
+   render json: single_student 
+  end
+
 end
